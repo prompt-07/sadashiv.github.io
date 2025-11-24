ca7420c7c8f69e1e564714ab372dcbfa642587aa
prompt-07 <65489912+prompt-07@users.noreply.github.com>
2025-11-24 09:05:41 +0000

    Deploying to gh-pages from @ prompt-07/sadashiv.github.io@b6f8f564ed5efbe62febcdb0e1bac865dd706adf 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "prompt-07/al-folio",
+    "data-repo": "prompt-07/sadashiv.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
